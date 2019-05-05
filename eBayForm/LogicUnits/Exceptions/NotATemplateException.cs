a0a69a44809b68dc65e8eed8493dd56cf36b2713
german_firsov <german.firsov@protonmai.com>
2019-05-05 23:20:17 +0200

    Temporary exceptions

---
+++ eBayForm/LogicUnits/Exceptions/NotATemplateException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace eBayForm.LogicUnits.Exceptions
+{
+    class NotATemplateException : Exception
+    {
+        public NotATemplateException(string message) : base (message) { }
+    }
+}
---
+++ eBayForm/LogicUnits/Exceptions/UnknownTemplateException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace eBayForm.LogicUnits.Exceptions
+{
+    class UnknownTemplateException : Exception
+    {
+        public UnknownTemplateException(string message) : base(message) { }
+    }
+}
