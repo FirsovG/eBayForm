0d50be2cd8ad294d6d52f162d99e9fb1644e6505
german_firsov <german.firsov@protonmai.com>
2019-05-06 00:05:51 +0200

    Static class to manage message box

---
+++ eBayForm/LogicUnits/CustomMessageBox.cs
@@ -0,0 +1,29 @@
+﻿using eBayForm.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace eBayForm.LogicUnits
+{
+    public static class CustomMessageBox
+    {
+        public static bool? Show(string message)
+        {
+            OwnMessageBox messageBox = new OwnMessageBox(message);
+            messageBox.ShowDialog();
+
+            return messageBox.DialogResult;
+        }
+
+        public static bool? Dialog(string message)
+        {
+            YNDialog messageBox = new YNDialog(message);
+            messageBox.ShowDialog();
+
+            return messageBox.DialogResult;
+        }
+    }
+}
