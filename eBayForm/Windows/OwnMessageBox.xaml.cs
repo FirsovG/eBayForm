d98e61542063ea78224b505ba98bd4a0742bce16
german_firsov <german.firsov@protonmai.com>
2019-05-06 00:05:19 +0200

    Own message box

---
+++ eBayForm/Windows/OwnMessageBox.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace eBayForm.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для OwnMessageBox.xaml
+    /// </summary>
+    public partial class OwnMessageBox : Window
+    {
+        public OwnMessageBox(string message)
+        {
+            InitializeComponent();
+            Taskbar.Content = new DesignItems.Taskbar(this);
+            tbMessage.Text = message;
+        }
+    }
+}
---
+++ eBayForm/Windows/YNDialog.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace eBayForm.Windows
+{
+    public partial class YNDialog : Window
+    {
+        public YNDialog(string message)
+        {
+            InitializeComponent();
+            Taskbar.Content = new DesignItems.Taskbar(this);
+            tbMessage.Text = message;
+        }
+
+        private void BtnYes_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+    }
+}
