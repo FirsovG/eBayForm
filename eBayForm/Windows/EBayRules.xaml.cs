66c9bb5611c2b651067bdcdfb598e57edd660908
german_firsov <german.firsov@protonmai.com>
2019-06-01 01:38:01 +0200

    added window with eBayRules

---
+++ eBayForm/Windows/EBayRules.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace eBayForm.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для EBayRules.xaml
+    /// </summary>
+    public partial class EBayRules : Window
+    {
+        public EBayRules()
+        {
+            InitializeComponent();
+            Taskbar.Content = new DesignItems.Taskbar(this);
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
+        }
+    }
+}
