85c4733777c72fb7a4b44b7d47e9c013be71bd01
german_firsov <german.firsov@protonmai.com>
2019-06-01 01:35:58 +0200

    concatenated struct for tags

---
+++ eBayForm/LogicUnits/HtmlTagElement.cs
@@ -0,0 +1,27 @@
+﻿
+namespace eBayForm.LogicUnits
+{
+    public struct HtmlTagElement
+    {
+        public bool IsInList { get; set; }
+        public string Element { get; set; }
+        public string Value { get; set; }
+        public string Link { get; set; }
+
+        public HtmlTagElement(bool isInList, string element, string value)
+        {
+            IsInList = isInList;
+            Element = element;
+            Value = value.Trim();
+            Link = null;
+        }
+
+        public HtmlTagElement(bool isInList, string element, string value, string link)
+        {
+            IsInList = isInList;
+            Element = element;
+            Value = value.Trim();
+            Link = link;
+        }
+    }
+}
