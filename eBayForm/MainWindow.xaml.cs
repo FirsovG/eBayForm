16a5b9e12571c3e1ea7a72c6c88f596322aa5f7b
german_firsov <german.firsov@protonmai.com>
2019-07-15 07:00:01 +0200

    Added new packages

--- eBayForm/MainWindow.xaml.cs
+++ eBayForm/MainWindow.xaml.cs
@@ -10,22 +10,42 @@ using System.Windows.Input;
 
 namespace eBayForm
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
+
+    public delegate void DataChangedDelegate(object sender);
+
     public partial class MainWindow : Window
     {
         PropertiesToolBox toolBox;
         StylesToolBox stylesToolBox;
         LogicController lc;
+        Phone phone;
         public static RoutedCommand cmdSaveChanges = new RoutedCommand();
+
+        bool documentIsChanged = false;
+        // TODO: export file
+        //bool wasExported = false;
 
         public MainWindow()
         {
             InitializeComponent();
+            this.Name = "MainWindow";
             Taskbar.Content = new DesignItems.Taskbar(this);
             lc = new LogicController();
             wbWorkspace.Navigating += WbWorkspace_Navigating;
+
+            if(lc.Templates != null)
+            {
+                foreach (Button button in ElementFinder.FindVisualChildren<Button>(wpTemplates))
+                {
+                    for (int i = 0; i < lc.Templates.Length; i++)
+                    {
+                        if (button.Name == lc.Templates[i])
+                        {
+                            button.Visibility = Visibility.Visible;
+                        }
+                    }
+                }
+            }
         }
         
 
@@ -42,23 +62,49 @@ namespace eBayForm
         public void ShowWorkspace(string htmlCode)
         {
             wbWorkspace.NavigateToString(htmlCode);
+            
+            if (phone == null)
+            {
+                phone = new Phone(btnShowPhone);
+            }
+            phone.wbWorkspace.NavigateToString(htmlCode);
+            spPhoneButtons.Visibility = Visibility.Visible;
+
             wbWorkspace.Visibility = Visibility.Visible;
             spButtons.Visibility = Visibility.Visible;
+
             cmdSaveChanges.InputGestures.Add(new KeyGesture(Key.S, ModifierKeys.Control));
-            stylesToolBox = new StylesToolBox(lc, wbWorkspace, btnShowToolBox);
+            
+            stylesToolBox = new StylesToolBox(this, lc, wbWorkspace, btnShowStyleBox);
             stylesToolBox.Show();
-            toolBox = new PropertiesToolBox(lc, wbWorkspace, btnShowToolBox);
+            stylesToolBox.DataChanged += DataChangedHandler; 
+            toolBox = new PropertiesToolBox(this, lc, wbWorkspace, btnShowToolBox);
             toolBox.Show();
+            toolBox.DataChanged += DataChangedHandler;
+
+            CustomMessageBox.ShowTipp("Ctrl+S to save changes");
+
             tabControll.SelectedIndex = 0;
         }
 
-        private void SaveChanges()
+        private void DataChangedHandler(object sender)
+        {
+            // Log with sender :: Later
+            if (!documentIsChanged)
+            {
+                documentIsChanged = true;
+            }
+        }
+
+        public void SaveChanges()
         {
             if (lc.Document != null && toolBox != null && stylesToolBox != null)
             {
                 List<HtmlTagElement> htmlTags = toolBox.SaveChanges();
-                List<TagStyleElement> styleElements = stylesToolBox.SaveChanges();
-                wbWorkspace.NavigateToString(lc.SaveChanges(htmlTags, styleElements));
+                List<StyleTagElement> styleElements = stylesToolBox.SaveChanges();
+                string htmlCode = lc.SaveChanges(htmlTags, styleElements);
+                wbWorkspace.NavigateToString(htmlCode);
+                phone.wbWorkspace.NavigateToString(htmlCode);
             }
         }
 
@@ -74,34 +120,45 @@ namespace eBayForm
 
         public void ExportCheck()
         {
-            if (lc.Document != null && toolBox != null && stylesToolBox != null)
+            if(documentIsChanged)
             {
-                bool? result = CustomMessageBox.Dialog("Do you want to export the file?");
-                if (result == true)
+                if (lc.Document != null && toolBox != null && stylesToolBox != null)
                 {
-                    SaveFileDialog fileDialog = new SaveFileDialog();
-                    fileDialog.Filter = "html files (*.html)|*.html";
-                    bool? dialogResult = fileDialog.ShowDialog();
-
-                    if (dialogResult == true)
+                    bool? result = CustomMessageBox.Dialog("Do you want to export the file?");
+                    if (result == true)
                     {
-                        lc.Export(fileDialog.FileName);
+                        SaveFileDialog fileDialog = new SaveFileDialog();
+                        fileDialog.Filter = "Html-Files (*.html)|*.html";
+                        bool? dialogResult = fileDialog.ShowDialog();
+
+                        if (dialogResult == true)
+                        {
+                            lc.Export(fileDialog.FileName);
+                        }
                     }
+                    
+                    wbWorkspace.Visibility = Visibility.Hidden;
+                    spButtons.Visibility = Visibility.Hidden;
+                    spPhoneButtons.Visibility = Visibility.Hidden;
+
+                    phone.Hide();
+                    toolBox.Close();
+                    stylesToolBox.Close();
+                    documentIsChanged = false;
                 }
-                toolBox.Close();
-                stylesToolBox.Close();
             }
         }
 
         private void BtnExportFile_Click(object sender, RoutedEventArgs e)
         {
             SaveFileDialog fileDialog = new SaveFileDialog();
-            fileDialog.Filter = "html files (*.html)|*.html";
+            fileDialog.Filter = "Html-Files (*.html)|*.html";
             bool? dialogResult = fileDialog.ShowDialog();
 
             if (dialogResult == true)
             {
                 lc.Export(fileDialog.FileName);
+                documentIsChanged = false;
             }
         }
 
@@ -132,6 +189,7 @@ namespace eBayForm
                 {
                     string htmlCode = await lc.ImportHtmlAsync(openFileDialog.FileName);
                     ShowWorkspace(htmlCode);
+                    this.Title = lc.Templatename + " : " + "eBayForm";
                 }
             }
             catch (TemplateException exception)
@@ -143,7 +201,19 @@ namespace eBayForm
         private void BtnShowToolBox_Click(object sender, RoutedEventArgs e)
         {
             toolBox.Show();
-            ((Button)sender).Visibility = Visibility.Collapsed;
+            (sender as Button).Visibility = Visibility.Collapsed;
+        }
+
+        private void BtnShowStyleBox_Click(object sender, RoutedEventArgs e)
+        {
+            stylesToolBox.Show();
+            (sender as Button).Visibility = Visibility.Collapsed;
+        }
+
+        private void BtnShowPhone_Click(object sender, RoutedEventArgs e)
+        {
+            phone.Show();
+            (sender as Button).Visibility = Visibility.Collapsed;
         }
         #endregion
 
@@ -180,6 +250,8 @@ namespace eBayForm
 
                     string htmlCode = lc.LoadTemplate(templatename, configurationKeyValues);
                     ShowWorkspace(htmlCode);
+                    this.Title = lc.Templatename + " : " + "eBayForm";
+                    documentIsChanged = true;
                 }
             }
         }
